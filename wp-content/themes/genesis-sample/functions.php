81ad9c080eb2fa0d184c1421df8b362c0cec7afd
Jen Schoeller <jenschoeller@Jens-MacBook-Pro.local>
2015-09-08 14:03:22 -0500

    header and sidebar

--- wp-content/themes/genesis-sample/functions.php
+++ wp-content/themes/genesis-sample/functions.php
@@ -16,7 +16,7 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
-	wp_enqueue_script( 'bower-jquery', get_stylesheet_directory() . '/bower_components/jquery/dist/jquery.min.js', array(), CHILD_THEME_VERSION );
+	wp_enqueue_script( 'bower-jquery', get_stylesheet_directory_uri() . '/assets/js/bower_components/jquery/dist/jquery.min.js', array(), CHILD_THEME_VERSION );
 }
 
 //* Add HTML5 markup structure
@@ -51,5 +51,70 @@ function sp_search_button_text( $text ) {
        return esc_attr( 'Go' );
 }
 
+
+
+//* Remove Title from Homepage
+add_action( 'get_header', 'remove_titles_from_pages' );
+function remove_titles_from_pages() {
+    if ( is_page(array(home) ) ) {
+        remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+    }
+}
+
+
+
+
+//* Remove the site footer
+remove_action( 'genesis_footer', 'genesis_footer_markup_open', 5 );
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+remove_action( 'genesis_footer', 'genesis_footer_markup_close', 15 );
+
+//* Customize the site footer
+add_action( 'genesis_footer', 'bg_custom_footer' );
+function bg_custom_footer() { ?>
+
+	<div class="site-footer">
+		<div class="wrap">		
+			<ul class="social-footer">
+				<li><a href="https://www.facebook.com/pages/Plunketts-Pest-Control/173200970941" target="_blank"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://twitter.com/plunkettspest" target="_blank"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="https://twitter.com/plunkettspest" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+			</ul>	
+			<p class="copyright-text">Copyright 2015. All Rights Reserved.</p>
+		</div>
+	</div>
+
+<?php
+}
+
+
+// Add Edit Featured Image Size
+add_image_size( 'blog-sidebar-thumbnails', 280, 170 );
+
+function custom_excerpt($text) {  // custom 'read more' link
+   if (strpos($text, '[...]')) {
+      $excerpt = strip_tags(str_replace('[...]', '... <a href="'.get_permalink().'">read more...</a>', $text), "<a>");
+   } else {
+      $excerpt = '<p>' . $text . '&nbsp;&nbsp;<a href="'.get_permalink().'">read more...</a></p>';
+   }
+   return $excerpt;
+}
+add_filter('the_excerpt', 'custom_excerpt');
+
+
+
+// Add CTA Shortcode
+function gruen_cta_shortcode() {
+	
+	$cta = '<a href="#" target="_blank" class="cta-quote-btn">';
+	$cta .= '<div class="cta-shortcode" role="alert">';
+	$cta .= '<i class="fa fa-envelope"></i>';
+	$cta .= '<h2>Request a Quote</h2>';
+	$cta .= '</div>';
+	$cta .= '</a>';
+	return $cta;
+	
+};
+add_shortcode( 'cta', 'gruen_cta_shortcode' );
 
 
