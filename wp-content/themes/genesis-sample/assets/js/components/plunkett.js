c2b16c14175fafc7b3085327db9e67d91634b2f7
Scott Hause <scott.hause@blueearth.net>
2015-09-22 12:25:21 -0500

    nav toggle

--- wp-content/themes/genesis-sample/assets/js/components/plunkett.js
+++ wp-content/themes/genesis-sample/assets/js/components/plunkett.js
@@ -1,7 +1,5 @@
 $( document ).ready(function($) {
-	console.log('ready');
     $('.toggle-container').click(function() {
-    	alert('hey');
     	$('.widget-area.header-widget-area,#menu-main').slideToggle('slow');
     });
 });
